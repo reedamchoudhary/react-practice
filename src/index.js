247127c115c8fdea6b6c972e652ef2b390b28ee1
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-08 12:11:14 +0530

    indentation

--- src/index.js
+++ src/index.js
@@ -5,24 +5,9 @@ import Card from './Card'
 import App from "./App"
 import data from "./data"
 
-// const ncard = (val) =>{
-//   return (
-//     <Card 
-//     key = {val.id}
-//     imagesrc = {val.imagesrc}
-//     link =  {val.link}
-//     title = {val.title}
-//     name = {val.name}
-//   />
-//   )
-// }
-
 ReactDOM.render(
   <>
-  {/* <h1>Following are the most popular series:</h1>
   
-  
-  {data.map(ncard)} */}
 
   <App />
   
